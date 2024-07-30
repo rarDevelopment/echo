445e3acf9616aad71a2130b21faeb414de40a39d
Robert R <rarDevelopment@users.noreply.github.com>
2024-07-30 19:46:47 -0400

    split functionality into functions to reduce duplicate code

--- index.js
+++ index.js
@@ -4,22 +4,17 @@ import posters from "./lib/posters/index.js";
 import config from "./config.js";
 
 const echoPath = process.argv[1].replace("index.js", "");
-
-const url = "https://rardk64.com/rss/feed-configs/json/";
+const dataFilePath = `${echoPath}data`;
 
 const cachedFeedItems = {};
 
-const allFeedConfigsResponse = await fetch(url);
+const feedConfigsJsonUrl = "https://rardk64.com/rss/feed-configs/json/";
+
+const allFeedConfigsResponse = await fetch(feedConfigsJsonUrl);
 const allFeedConfigs = await allFeedConfigsResponse.json();
 
 const args = process.argv.slice(2);
-//let INIT_MODE = args.includes("init");
 const DRY_MODE = args.includes("dry");
-
-// if (DRY_MODE && INIT_MODE) {
-//   console.log("🚨 You cannot run Echo with init mode AND dry mode enabled at the same time");
-//   process.exit();
-// }
 
 if (DRY_MODE) {
   console.log("🌵 Running in dry mode, no posts will be created");
@@ -27,25 +22,16 @@ if (DRY_MODE) {
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-if (!fs.existsSync(`${echoPath}data`)) {
-  fs.mkdirSync(`${echoPath}data`);
+if (!fs.existsSync(dataFilePath)) {
+  fs.mkdirSync(dataFilePath);
   console.log("📁 Data folder created!");
 }
 
 for (const feedConfig of allFeedConfigs) {
-  const feedFile = buildFeedFileName(feedConfig);
+  const feedFileName = buildFeedFileName(feedConfig);
+  const feedFilePath = `${echoPath}data/${feedFileName}`;
 
-  if (!fs.existsSync(`${echoPath}data/${feedFile}`)) {
-    // if (!DRY_MODE) {
-    //   INIT_MODE = true;
-    // }
-    await fs.writeFile(`${echoPath}data/${feedFile}`, JSON.stringify([], "", 2), { flag: "wx" }, (err) => {
-      if (err) {
-        throw err;
-      }
-      console.log(`✅ ${feedConfig.feed_display_name} data file created!`);
-    });
-  }
+  createFileIfNotExists(feedFilePath, feedFileName);
 
   console.log(`⚙️ Fetching for ${feedConfig.feed_display_name}`);
   let items = await getFeedItems(feedConfig.feed_url, false, null); // NOTE: have to change that hard-coded false if I want to use json feeds
@@ -60,7 +46,7 @@ for (const feedConfig of allFeedConfigs) {
     break;
   }
 
-  const existingIds = JSON.parse(fs.readFileSync(`${echoPath}data/${feedFile}`, "utf8"));
+  const existingIds = JSON.parse(fs.readFileSync(feedFilePath, "utf8"));
 
   if (existingIds.length > 0) {
     items = items.filter((item) => {
@@ -70,12 +56,10 @@ for (const feedConfig of allFeedConfigs) {
 
   //now filter out the items before the feed was created that weren't filtered above
 
-  let newExistingItemsToAdd = false;
   const feedConfigCreatedDate = new Date(feedConfig.created_at);
   for (const item of items) {
     if (new Date(item.isoDate) < feedConfigCreatedDate) {
       existingIds.push(item.guid);
-      newExistingItemsToAdd = true;
     }
   }
 
@@ -87,38 +71,26 @@ for (const feedConfig of allFeedConfigs) {
 
   const newIds = items.map((i) => i.guid);
 
-  //   if (newExistingItemsToAdd && !DRY_MODE) {
-  //     fs.writeFileSync(`${echoPath}data/${feedFile}`, JSON.stringify([...newIds, ...existingIds], "", 2));
-  //   }
-
   if (!items.length) {
     console.log(`❌ No new items found for ${feedConfig.feed_display_name}`);
     //update the file anyway, since we want the file to be up to date with all existing ids
-    fs.writeFileSync(`${echoPath}data/${feedFile}`, JSON.stringify([...existingIds], "", 2));
+    updateFileWithIds([...existingIds], feedFilePath);
     continue;
   }
 
   //update file with new ids (as well as existing ids) if we're not in dry mode so that we don't post the same items again
   if (!DRY_MODE) {
-    console.log("writing", [...newIds, ...existingIds]);
-    fs.writeFileSync(`${echoPath}data/${feedFile}`, JSON.stringify([...newIds, ...existingIds], "", 2));
+    updateFileWithIds([...newIds, ...existingIds], feedFilePath);
   }
-
-  //   if (INIT_MODE) {
-  //     console.log(`⚙️ Echo initialised for ${feedConfig.feed_display_name}!`);
-  //     continue;
-  //   }
-  //   INIT_MODE = args.includes("init"); // TODO: remove this if I refactor how feeds are tracked
 
   for (const item of items) {
     const formattedMessageObject = formatMessage(feedConfig.message_template, item);
-    //console.log("formatted", formattedMessageObject);
+
     if (DRY_MODE) {
       console.log(
         `✅ Will create ${feedConfig.feed_display_name} post for ${formattedMessageObject.date}\n\n${formattedMessageObject.content}`
       );
     } else {
-      //console.log("config", feedConfig);
       await delay(2000);
       await posters[feedConfig.service_type](feedConfig, formattedMessageObject, config);
     }
@@ -130,12 +102,27 @@ process.exit();
 
 ///////// END OF SCRIPT /////////
 
+async function createFileIfNotExists(feedFilePath, feedFileName) {
+  if (!fs.existsSync(feedFilePath)) {
+    await fs.writeFile(feedFilePath, JSON.stringify([], "", 2), { flag: "wx" }, (err) => {
+      if (err) {
+        throw err;
+      }
+      console.log(`✅ ${feedFileName} data file created!`);
+    });
+  }
+}
+
+function updateFileWithIds(ids, feedFilePath) {
+  fs.writeFileSync(feedFilePath, JSON.stringify(ids, "", 2));
+}
+
 async function getFeedItems(feed, isJson, customFields) {
   if (cachedFeedItems[feed]) {
     console.log("using cached items", cachedFeedItems);
     return cachedFeedItems[feed];
   }
-  
+
   if (isJson) {
     const res = await fetch(feed);
     const feedData = await res.json();
