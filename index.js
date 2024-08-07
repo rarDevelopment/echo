d266d53fa9653fcf3ed12a28cc3f9a00b5c6b202
Robert R <rarDevelopment@users.noreply.github.com>
2024-08-06 22:36:16 -0400

    remove unused code, handle errors, refactor

--- index.js
+++ index.js
@@ -51,98 +51,108 @@ for (const feedConfig of allFeedConfigs) {
 
   const feedFileName = buildFeedFileName(feedConfig);
   const feedFilePath = `${echoPath}data/${feedFileName}`;
+  try {
+    await createFileIfNotExists(feedFilePath, feedFileName);
 
-  await createFileIfNotExists(feedFilePath, feedFileName);
+    console.log(`⚙️ Fetching for ${feedConfig.feed_display_name}`);
+    let items = await getFeedItems(feedConfig.feed_url, false, null); // NOTE: have to change that hard-coded false if I want to use json feeds
 
-  console.log(`⚙️ Fetching for ${feedConfig.feed_display_name}`);
-  let items = await getFeedItems(feedConfig.feed_url, false, null); // NOTE: have to change that hard-coded false if I want to use json feeds
-
-  if (items.length === 0) {
-    console.log(`0️⃣ No items found for ${feedConfig.feed_display_name}`);
-    continue;
-  }
-
-  if (!getGuid(items[0])) {
-    console.log(`❌ No ID found for item in ${feedConfig.feed_display_name}, skipping. 👀 Does this item have a guid?`);
-    break;
-  }
-
-  let existingIds = JSON.parse(await fs.readFile(feedFilePath, "utf8"));
-
-  //filter out the items from before the feed was created
-  const connectionCreatedDate = new Date(feedConfig.connection_created_at);
-  for (const item of items) {
-    if (new Date(item.isoDate) < connectionCreatedDate && !existingIds.includes(getGuid(item))) {
-      existingIds.push(getGuid(item));
+    if (items.length === 0) {
+      console.log(`0️⃣ No items found for ${feedConfig.feed_display_name}`);
+      continue;
     }
-  }
 
-  //update the file now anyway, since we want the file to be up to date with all existing ids
-  await updateFileWithIds([...existingIds], feedFilePath);
-
-  let manualPostsToDo = [];
-  if (isWebhookFeedConfig) {
-    manualPostsToDo = manualPostsWebhooks.filter(
-      (p) => p.config_id === feedConfig.config_id && p.webhook_id === feedConfig.webhook_id
-    );
-  } else if (isSocialFeedConfig) {
-    manualPostsToDo = manualPostsSocials.filter(
-      (p) =>
-        p.config_id === feedConfig.config_id &&
-        p.service_type === feedConfig.service_type &&
-        p.user_id === feedConfig.user_id
-    );
-  }
-
-  //filter out existing ids that will be manually posted
-  existingIds = existingIds.filter((id) => {
-    //if we find a manual post with the same guid, we don't want that in existingIds because we want to post it
-    const needToPost = manualPostsToDo.find((p) => p.feed_item_guid === id);
-    return !needToPost; //negated because we want to exclude the id
-  });
-
-  if (existingIds.length > 0) {
-    items = items.filter((item) => {
-      return !existingIds.includes(getGuid(item));
-    });
-  }
-
-  const newIds = items.map((i) => getGuid(i));
-
-  if (!items.length) {
-    console.log(`❌ No new items found for ${feedFileName}`);
-    continue;
-  }
-
-  //update file with new ids (as well as existing ids) if we're not in dry mode so that we don't post the same items again
-  if (!DRY_MODE) {
-    await updateFileWithIds([...newIds, ...existingIds], feedFilePath);
-  }
-
-  for (const item of items) {
-    const formattedMessageObject = formatMessage(
-      feedConfig.message_template,
-      item,
-      characterLimits[feedConfig.service_type]
-    );
-
-    if (DRY_MODE || DEV_MODE) {
+    if (!getGuid(items[0])) {
       console.log(
-        `✅ Will create ${feedConfig.feed_display_name} post for ${formattedMessageObject.date}\n\n${formattedMessageObject.content}`
+        `❌ No ID found for item in ${feedConfig.feed_display_name}, skipping. 👀 Does this item have a guid?`
       );
-    } else {
-      await delay(2000);
-      await posters[feedConfig.service_type](feedConfig, formattedMessageObject, config);
-      if (manualPostsToDo.length > 0) {
-        if (manualPostsToDo.find((p) => p.feed_item_guid === getGuid(item) && p.config_id === feedConfig.config_id)) {
-          await markManualPostAsProcessed(getGuid(item), feedConfig, isWebhookFeedConfig, isSocialFeedConfig);
-        } else {
-          console.log("didn't find that one for some reason", getGuid(item), feedConfig.config_id, manualPostsWebhooks);
+      break;
+    }
+
+    let existingIds = JSON.parse(await fs.readFile(feedFilePath, "utf8"));
+
+    //filter out the items from before the feed was created
+    const connectionCreatedDate = new Date(feedConfig.connection_created_at);
+    for (const item of items) {
+      if (new Date(item.isoDate) < connectionCreatedDate && !existingIds.includes(getGuid(item))) {
+        existingIds.push(getGuid(item));
+      }
+    }
+
+    //update the file now anyway, since we want the file to be up to date with all existing ids
+    await updateFileWithIds([...existingIds], feedFilePath);
+
+    let manualPostsToDo = [];
+    if (isWebhookFeedConfig) {
+      manualPostsToDo = manualPostsWebhooks.filter(
+        (p) => p.config_id === feedConfig.config_id && p.webhook_id === feedConfig.webhook_id
+      );
+    } else if (isSocialFeedConfig) {
+      manualPostsToDo = manualPostsSocials.filter(
+        (p) =>
+          p.config_id === feedConfig.config_id &&
+          p.service_type === feedConfig.service_type &&
+          p.user_id === feedConfig.user_id
+      );
+    }
+
+    //filter out existing ids that will be manually posted
+    existingIds = existingIds.filter((id) => {
+      //if we find a manual post with the same guid, we don't want that in existingIds because we want to post it
+      const needToPost = manualPostsToDo.find((p) => p.feed_item_guid === id);
+      return !needToPost; //negated because we want to exclude the id
+    });
+
+    if (existingIds.length > 0) {
+      items = items.filter((item) => {
+        return !existingIds.includes(getGuid(item));
+      });
+    }
+
+    const newIds = items.map((i) => getGuid(i));
+
+    if (!items.length) {
+      console.log(`❌ No new items found for ${feedFileName}`);
+      continue;
+    }
+
+    //update file with new ids (as well as existing ids) if we're not in dry mode so that we don't post the same items again
+    if (!DRY_MODE) {
+      await updateFileWithIds([...newIds, ...existingIds], feedFilePath);
+    }
+
+    for (const item of items) {
+      const formattedMessageObject = formatMessage(
+        feedConfig.message_template,
+        item,
+        characterLimits[feedConfig.service_type]
+      );
+
+      if (DRY_MODE || DEV_MODE) {
+        console.log(
+          `✅ Will create ${feedConfig.feed_display_name} post for ${formattedMessageObject.date}\n\n${formattedMessageObject.content}`
+        );
+      } else {
+        await delay(2000);
+        await posters[feedConfig.service_type](feedConfig, formattedMessageObject, config);
+        if (manualPostsToDo.length > 0) {
+          if (manualPostsToDo.find((p) => p.feed_item_guid === getGuid(item) && p.config_id === feedConfig.config_id)) {
+            await markManualPostAsProcessed(getGuid(item), feedConfig, isWebhookFeedConfig, isSocialFeedConfig);
+          } else {
+            console.log(
+              "didn't find that one for some reason",
+              getGuid(item),
+              feedConfig.config_id,
+              manualPostsWebhooks
+            );
+          }
         }
       }
     }
+    //console.log(`✅ ${items.length} items found for ${feedFileName}`);
+  } catch (err) {
+    console.error(`There was an error processing the feed ${feedFileName}: `, err);
   }
-  console.log(`✅ ${items.length} items found for ${feedFileName}`);
 }
 
 cachedFeedItems = [];
